1982386edbab1a28afa8e614f2071b1a29ba241d
Romain GONÇALVES <romain.goncalves@gmail.com>
2022-07-04 19:48:43 +0200

    Base get route

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var HTTP_PORT = 8000;
+
+app.listen(HTTP_PORT, () => {
+    console.log(`Server running on port ${HTTP_PORT}`);
+});
+
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
+
+// Fallback route
+app.use(function(req, res){
+    res.status(404);
+});
